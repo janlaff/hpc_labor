b932d135adc3db02488355cdf4a7be8cd3b75326
Jan Lafferton <jan@lafferton.de>
2020-10-17 12:43:35 +0200

    hello world a + b

---
+++ uebung01/hello_world_b.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main(int argc, char **argv) {
+    #pragma omp parallel num_threads(4)
+    {
+        printf("Hello World from thread %d of %d\n", omp_get_thread_num(), omp_get_num_threads());
+    }
+
+    return 0;
+}
